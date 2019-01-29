5c7de6ca3727fb65e16d277097f667b928e75322
mrflark <joseph.sirna@gmail.com>
2019-01-29 08:04:51 -0700

    initial commit

---
+++ CS386_Project/Controllers/DataController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CS386_Project.Controllers
+{
+    public class DataController : Controller
+    {
+        [HttpPost]
+        public JsonResult Test(string param){
+            return Json(new {
+                StatusCode = 200,
+                Message = "param: " + param
+            });
+        }
+    }
+}
---
+++ CS386_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CS386_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
